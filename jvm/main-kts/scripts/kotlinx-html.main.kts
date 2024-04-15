01cab21a3c5afe6686b76bb76de0afc2c10493cf
Evgenii Mazhukin <evgenii.mazhukin@jetbrains.com>
2024-04-15 18:13:09 +0200

    basic use of kbb apis

--- jvm/main-kts/scripts/kotlinx-html.main.kts
+++ jvm/main-kts/scripts/kotlinx-html.main.kts
@@ -1,14 +1,42 @@
 #!/usr/bin/env kotlin
 
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
+@file:Repository("/home/Evgenii.Mazhukin/.m2")
+@file:Repository("https://repo.gradle.org/gradle/libs-releases")
+@file:Repository("https://repo1.maven.org/maven2")
+@file:DependsOn("org.jetbrains:kotlin-build-benchmarks:1.0-SNAPSHOT")
 
-import kotlinx.html.*; import kotlinx.html.stream.*; import kotlinx.html.attributes.*
+import org.jetbrains.kotlin.build.benchmarks.dsl.*
 
 val addressee = args.firstOrNull() ?: "World"
 
-print(createHTML().html {
-    body {
-        h1 { +"Hello, $addressee!" }
-    }
-})
+val bar = suite {
+    val buildSrc = changeableFile("buildSrc/BuildPropertiesExtKt")
+    val noArgs = arrayOf<String>()
 
+    scenario("\$jdkPrefix\${prefix}(buildSrc, Kotlin) add private fun") {
+        jdk = System.getenv("JDK_8")
+
+        step {
+            changeFile(buildSrc, TypeOfChange.ADD_PRIVATE_FUNCTION)
+            runTasks(*noArgs)
+        }
+    }
+
+    scenario("tt") {
+        jdk = System.getenv("JDK_8")
+
+        expectSlowBuild("reasons")
+        step {
+            changeFile(buildSrc, TypeOfChange.ADD_PRIVATE_FUNCTION)
+            runTasks(*noArgs)
+        }
+        step {
+            changeFile(buildSrc, TypeOfChange.ADD_PRIVATE_FUNCTION)
+            runTasks(*noArgs)
+        }
+    }
+}
+
+println(bar.toString())
+println(bar.scenarios.size)
+println(bar.scenarios.joinToString(" also "))
