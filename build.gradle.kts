f7a07a0b7d317f8023cec59d7cef366fab7c914d
Evgenii Mazhukin <evgenii.mazhukin@jetbrains.com>
2024-04-15 18:19:55 +0200

    with this sources become available. consider making a custom script runner with necessary dependencies instead? well that's a low level detail anyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,17 @@ plugins {
     kotlin("jvm") version "1.8.21"
 }
 
+repositories {
+    maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains:kotlin-build-benchmarks:1.0-SNAPSHOT")
+    implementation(kotlin("stdlib"))
+}
+
 val kotlinVersion: String by extra("1.8.21")
 val kotlinCoroutinesVersion: String by extra("1.7.0-RC")
 
